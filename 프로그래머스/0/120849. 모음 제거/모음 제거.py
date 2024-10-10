e90ed54eb84bd0977b9b5aa1489fdf9d78a03273
SongOh <83348645+ssh10032@users.noreply.github.com>
2024-10-10 10:32:54 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -1,6 +1,5 @@
 def solution(my_string):
     r_list = ['a', 'e', 'i', 'o', 'u']
-    for i in my_string:
-        if i in r_list:
+    for i in r_list:
             my_string = my_string.replace(i, '')
     return my_string
