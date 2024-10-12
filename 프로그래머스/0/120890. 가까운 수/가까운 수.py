4b549628b21d5e29b8d26bf08278b3efe84a37f6
SongOh <83348645+ssh10032@users.noreply.github.com>
2024-10-12 12:00:24 +0900

    [level 0] Title: 가까운 수, Time: 0.04 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.py
@@ -1,4 +1,3 @@
 def solution(array, n):
-    min_value = min([abs(i-n) for i in array])
-    v_list = [v for i, v in enumerate(array) if abs(v-n)==min_value]
-    return min(v_list)
+    array.sort(key = lambda x : (abs(x-n),x-n))
+    return array[0]
