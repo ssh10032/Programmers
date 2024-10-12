06e8253042d1cb6e466f39aef9a7ece73f852218
SongOh <83348645+ssh10032@users.noreply.github.com>
2024-10-12 12:10:24 +0900

    [level 0] Title: 369게임, Time: 0.01 ms, Memory: 9.82 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.py
+++ 프로그래머스/0/120891. 369게임/369게임.py
@@ -1,7 +1,2 @@
 def solution(order):
-    n_list = str(order)
-    count = 0
-    for i in n_list:
-        if i in ["3", "6", "9"]:
-            count+=1
-    return count
+    return sum(map(lambda x: str(order).count(x), ["3", "6", "9"]))
