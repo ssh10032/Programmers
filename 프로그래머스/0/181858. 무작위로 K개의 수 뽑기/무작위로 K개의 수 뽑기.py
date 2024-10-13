3bba5ce426497b57769e85945c043e054dd0a192
SongOh <83348645+ssh10032@users.noreply.github.com>
2024-10-13 13:05:11 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 2295.96 ms, Memory: 10.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
@@ -0,0 +1,12 @@
+def solution(arr, k):
+    # u_arr = list(set(arr))
+    u_arr = []
+    for i in arr:
+        if i not in u_arr:
+            u_arr.append(i)
+    if len(u_arr)>k:
+        return u_arr[:k]
+    elif len(u_arr)==k:
+        return u_arr
+    else:
+        return u_arr + [-1] * (k-len(u_arr))
