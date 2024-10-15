c636f4735183441b5af4a1f19785aaaf93649268
SongOh <83348645+ssh10032@users.noreply.github.com>
2024-10-15 13:13:55 +0900

    [level 0] Title: 숫자 찾기, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.py
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.py
@@ -1,5 +1,5 @@
 def solution(num, k):
-    n_list = list(''.join(str(num).split()))
-    if str(k) in n_list:
-        return n_list.index(str(k))+1
+    for i, v in enumerate(str(num)):
+        if v==str(k):
+            return i+1
     return -1
