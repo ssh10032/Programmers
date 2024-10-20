493a6db6e6c7c55321abff118ab2311919a19e7f
SongOh <83348645+ssh10032@users.noreply.github.com>
2024-10-20 19:28:03 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,18 +1,4 @@
 def solution(numbers):
-    if len(numbers)==2:
-        return numbers[0] * numbers[1]
-    minus_list = [abs(i) for i in numbers if i <0]
-    plust_list = [i for i in numbers if i >0]
-    minus_list.sort()
-    plust_list.sort()
-    if len(minus_list)>1:
-        m_max = minus_list[-2] * minus_list[-1]
-    else:
-        m_max = 0
-        
-    if len(plust_list)>1:
-        p_max = plust_list[-2] * plust_list[-1]
-    else:
-        p_max = 0
+    numbers.sort()
     
-    return p_max if p_max>m_max else m_max
+    return max(numbers[0] * numbers[1], numbers[-2]*numbers[-1])
