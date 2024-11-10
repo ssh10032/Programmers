3905dd9e572401954798ee097d5750b10176fbef
SongOh <83348645+ssh10032@users.noreply.github.com>
2024-11-10 17:49:00 +0900

    [D2] Title: 간단한 369게임, Time: 108 ms, Memory: 43,800 KB -BaekjoonHub

--- SWEA/D2/1926. 간단한 369게임/간단한 369게임.py
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.py
@@ -9,12 +9,6 @@ for num in range(1, T + 1):
     if '9' in str_num:
         clap_num+=  str_num.count('9')
     if clap_num==0:
-        if num == T:
-            print(str_num)
-        else :
-             print(str_num, end = ' ')
+        print(str_num, end = ' ')
     else:
-        if num == T:
-            print('-' * clap_num)
-        else:
-            print('-' * clap_num, end = ' ')
+        print('-' * clap_num, end = ' ')
