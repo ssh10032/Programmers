4c140d0dbb56b324da5bf63fb31fd13557558d7b
SongOh <83348645+ssh10032@users.noreply.github.com>
2024-12-15 17:11:00 +0900

    [level 1] Title: K번째수, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,7 +1,7 @@
 def solution(array, commands):
     answer = []
-    for i in range(len(commands)):
-        new_array = array[commands[i][0]-1:commands[i][1]]
+    for i, j, k in commands:
+        new_array = array[i-1:j]
         new_array.sort()
-        answer.append(new_array[commands[i][2]-1])
+        answer.append(new_array[k-1])
     return answer
