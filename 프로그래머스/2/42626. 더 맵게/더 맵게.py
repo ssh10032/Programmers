fba06b93f716a385fc4fc617fa647661705f6d36
SongOh <83348645+ssh10032@users.noreply.github.com>
2024-12-27 18:30:18 +0900

    [level 2] Title: 더 맵게, Time: 1943.70 ms, Memory: 51.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,20 @@
+import heapq
+def solution(scoville, K):
+    # print('Non-heap list is {}'.format(scoville))
+    heapq.heapify(scoville)
+    # print('heapify list is {}'.format(scoville))
+    try_num = 0
+    new_v = 0
+    while scoville[0]<K:
+        if len(scoville)==1:
+            print(all(scoville)<K)
+            print(try_num)
+            print(new_v)
+            print(scoville)
+            return -1
+        v1 = heapq.heappop(scoville)
+        v2 = heapq.heappop(scoville)
+        new_v = v1 + v2 * 2
+        try_num+=1
+        heapq.heappush(scoville, new_v)            
+    return try_num
